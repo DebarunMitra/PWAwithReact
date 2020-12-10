e1d66468d4fbd7d27e971d5db988cc887c71fb54
Debarun Mitra <50083511+DebarunMitra@users.noreply.github.com>
2020-12-10 22:33:17 +0530

    feat: :rocket: weather app setup done with basic design

---
+++ client/src/api/fetchWeather.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import {API_KEY, URL} from './env';
+
+export const fetchWeather = async (query) => {
+    const {data} = await axios.get(URL, {
+        params: {
+            q: query,
+            units: 'metric',
+            APPID: API_KEY,        }
+    });
+
+    return data;
+}
